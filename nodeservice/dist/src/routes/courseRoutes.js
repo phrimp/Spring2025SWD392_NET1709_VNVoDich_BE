46ffca3249e401f9a6b7ff86a9ced7f9ec11f3cb
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2025-02-09 16:15:47 +0700

    API for Lesson

--- nodeservice/dist/src/routes/courseRoutes.js
+++ nodeservice/dist/src/routes/courseRoutes.js
@@ -12,5 +12,8 @@ router.get("/", courseController_1.getCourses);
 router.get("/:id", courseController_1.getCourse);
 router.post("/", courseController_1.createCourse);
 router.put("/:id", upload.single("image"), courseController_1.updateCourse);
+router.put("/:courseId/add-lesson", courseController_1.addLessonToCourse);
+router.put("/:courseId/update-lesson/:lessonId", courseController_1.updateLesson);
+router.delete("/:courseId/delete-lesson/:lessonId", courseController_1.deleteLesson);
 router.delete("/:id", courseController_1.deleteCourse);
 exports.default = router;
