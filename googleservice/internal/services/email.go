6fb6b4b0001a9b0e29f189065a61434015498d1e
Phrimp <phien.nt@thdcybersecurity.xyz>
2025-02-16 00:48:41 +0700

    feat: email service/payment service built complete

--- googleservice/internal/services/email.go
+++ googleservice/internal/services/email.go
@@ -1,1 +1,36 @@
 package services
+
+import (
+	"fmt"
+	"google-service/internal/config"
+	"net/smtp"
+	"strings"
+)
+
+type EmailService struct {
+	config *config.SMTPConfig
+}
+
+func NewEmailService(_config *config.SMTPConfig) *EmailService {
+	return &EmailService{config: _config}
+}
+
+func (e *EmailService) SendEmail(title, body string, receipient []string) error {
+	to := receipient
+
+	message := []byte(fmt.Sprintf("To: %s\r\n"+
+		"Subject: %s\r\n"+
+		"\r\n"+
+		"%s\r\n", strings.Join(to, ","), title, body))
+
+	auth := smtp.PlainAuth("", e.config.Username, e.config.Password, e.config.Host)
+
+	addr := fmt.Sprintf("%s:$s", e.config.Host, e.config.Port)
+
+	err := smtp.SendMail(addr, auth, e.config.From, to, message)
+	if err != nil {
+		fmt.Printf("Error sending email: %s", err)
+		return err
+	}
+	return nil
+}
