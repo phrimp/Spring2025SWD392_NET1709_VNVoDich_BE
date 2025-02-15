629f64397375325d594f49afba3df8edce0b72bb
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2025-02-15 20:30:42 +0700

    Update Availability

--- nodeservice/src/controllers/availabilityController.ts
+++ nodeservice/src/controllers/availabilityController.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { Day, DayAvailability, PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 
 const prisma = new PrismaClient();
@@ -61,6 +61,79 @@ export const getTutorAvailability = async (
     res.json({
       message: "Availability retrieved successfully",
       data: availabilityData,
+    });
+  } catch (error: any) {
+    res.status(500).json({ message: "Error retrieving availability", error });
+  }
+};
+
+export const updateAvailability = async (req: Request, res: Response) => {
+  try {
+    const { userId, ...data } = req.body;
+
+    const tutor = await prisma.tutor.findUnique({
+      where: {
+        id: userId,
+      },
+      include: {
+        availability: true,
+      },
+    });
+
+    if (!tutor) {
+      res.json({
+        message: "Tutor not found",
+        data: null,
+      });
+      return;
+    }
+
+    const availabilityData = Object.entries(data).flatMap(
+      ([day, { isAvailable, startTime, endTime }]: any) => {
+        if (isAvailable) {
+          const baseDate = new Date().toISOString().split("T")[0];
+
+          return [
+            {
+              day: day.toUpperCase() as Day,
+              startTime: new Date(`${baseDate}T${startTime}:00Z`),
+              endTime: new Date(`${baseDate}T${endTime}:00Z`),
+            },
+          ];
+        }
+        return [];
+      }
+    );
+
+    let updatedAvailability;
+    if (tutor.availability) {
+      updatedAvailability = await prisma.availability.update({
+        where: {
+          id: tutor.availability.id,
+        },
+        data: {
+          timeGap: data.timeGap,
+          days: {
+            deleteMany: {},
+            create: availabilityData,
+          },
+        },
+      });
+    } else {
+      updatedAvailability = await prisma.availability.create({
+        data: {
+          tutor_id: tutor.id,
+          timeGap: data.timeGap,
+          days: {
+            create: availabilityData,
+          },
+        },
+      });
+    }
+
+    res.json({
+      message: "Availability updated successfully",
+      data: updatedAvailability,
     });
   } catch (error: any) {
     res.status(500).json({ message: "Error retrieving availability", error });
--- nodeservice/src/routes/availabilityRoutes.ts
+++ nodeservice/src/routes/availabilityRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { getTutorAvailability } from "../controllers/availabilityController";
+import {
+  getTutorAvailability,
+  updateAvailability,
+} from "../controllers/availabilityController";
 import tutorAuth from "../middleware/tutorAuth";
 
 const router = Router();
 
 router.get("/", tutorAuth, getTutorAvailability);
+router.put("/update", tutorAuth, updateAvailability);
 
 export default router;
