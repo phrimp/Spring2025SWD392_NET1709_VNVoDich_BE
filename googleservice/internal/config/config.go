104d013dcb950385c10121c35da9217882606f3b
Phrimp <phien.nt@thdcybersecurity.xyz>
2025-02-18 02:10:55 +0700

    refactor: move env var to docker-compose

--- googleservice/internal/config/config.go
+++ googleservice/internal/config/config.go
@@ -4,8 +4,6 @@ import (
 	"os"
 	"strconv"
 	"time"
-
-	"github.com/joho/godotenv"
 )
 
 type Config struct {
@@ -33,10 +31,6 @@ type JWTConfig struct {
 	Secret        string
 	ExpiresIn     time.Duration
 	RefreshSecret string
-}
-
-func init() {
-	godotenv.Load(".env")
 }
 
 // New creates a new Config instance with values from environment variables
