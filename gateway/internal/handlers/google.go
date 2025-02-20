17d8d675348604363ed5fbe38f1d2780deed15e0
Phrimp <phien.nt@thdcybersecurity.xyz>
2025-02-20 14:11:53 +0700

    fix: google state

--- gateway/internal/handlers/google.go
+++ gateway/internal/handlers/google.go
@@ -1,7 +1,10 @@
 package handlers
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"gateway/internal/routes"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/valyala/fasthttp"
@@ -19,11 +22,24 @@ func NewGoogleHandler(googleServiceURL string) *GoogleHandler {
 
 func (h *GoogleHandler) HandleLogin() fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		// Generate state in gateway
+		state := generateRandomState()
+
+		c.Cookie(&fiber.Cookie{
+			Name:     "oauth_state",
+			Value:    state,
+			Expires:  time.Now().Add(time.Minute * 5),
+			HTTPOnly: true,
+			Secure:   true,
+			SameSite: "Lax",
+			Path:     "/",
+		})
+
 		req := fasthttp.AcquireRequest()
 		resp := fasthttp.AcquireResponse()
 		defer fasthttp.ReleaseRequest(req)
 		defer fasthttp.ReleaseResponse(resp)
-		return routes.GoogleLoginRoute(req, resp, c, h.googleServiceURL+"/api/auth/google/login")
+		return routes.GoogleLoginRoute(req, resp, c, h.googleServiceURL+"/api/auth/google/login?state="+state)
 	}
 }
 
@@ -35,4 +51,10 @@ func (h *GoogleHandler) HandleCallback() fiber.Handler {
 		defer fasthttp.ReleaseResponse(resp)
 		return routes.GoogleLoginRoute(req, resp, c, h.googleServiceURL+"/api/auth/google/callback")
 	}
+}
+
+func generateRandomState() string {
+	b := make([]byte, 32)
+	rand.Read(b)
+	return base64.URLEncoding.EncodeToString(b)
 }
